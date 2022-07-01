13b99ea825f1989b023479586e7597d3b06a3f71
claudee <c.cabalquinto@gmail.com>
2022-07-01 15:53:51 -0700

    commit4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 /*----- constants -----*/
+const COLOR_LOOKUP = {
+    '1': 'red',
+    '-1': 'blue',
+    null: 'white'
+
+};
 
 /*----- app's state (variables) -----*/
 // Aray of 42 elements...
@@ -9,19 +15,49 @@ let gameStatus; // null -> game in play; 1/-1 player win; 'T' tie
 
 
 /*----- cached element references -----*/
+const circEls = document.querySelectorAll('#board > div');
+const msgEl = document.querySelector('h1');
 
 /*----- event listeners -----*/
+document.getElementById ('board').addEventListener('click', handleMove);
 
 /*----- functions -----*/
 init ();
 
 function init () {
-    board = new Array(42).fill(null);
+    //board = new Array(42).fill(null);
+    board = [
+        null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null,
+    ]
     turn = 1;
     gameStatus = null;
     render();
 }
 
-function render() {
 
+
+function render () {
+    circEls.forEach(function(circEl, idx){
+        circEl.style.backgroundColor = COLOR_LOOKUP[board[idx]];
+    });
+    renderMessage();
 }
+
+function handleMove (evt) {
+    console.log(evt.target);
+}
+
+function renderMessage() {
+    if (gameStatus === null) {
+      msgEl.innerHTML = `Player <span style="color: ${COLOR_LOOKUP[turn]}">${COLOR_LOOKUP[turn].toUpperCase()}</span>'s Turn`;
+    } else if (gameStatus === 'T') {
+      // Tie game
+    } else {
+      // Player has won!
+    }
+  }
